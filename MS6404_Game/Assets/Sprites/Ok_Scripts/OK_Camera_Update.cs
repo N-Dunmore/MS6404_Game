3ff4caf8d74c9bf6b907e1d81ab14fa63b42ceae
Unknown <rivermoredun@gmail.com>
2017-12-07 13:05:26 +0000

    Added Buttons
    Added basic buttons to move around scenes

---
+++ MS6404_Game/Assets/Sprites/Ok_Scripts/OK_Camera_Update.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OK_Camera_Update : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameObject.transform.position = gameObject.transform.parent.position;
+
+	}
+}
